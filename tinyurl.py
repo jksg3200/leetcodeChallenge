12bf3af1126b0d717b8739625b84696adfd881b4
jksg3200 <79204232+jksg3200@users.noreply.github.com>
2021-03-15 22:42:21 +0530

    Add files via upload

---
+++ tinyurl.py
@@ -0,0 +1,15 @@
+class Codec:
+
+	def encode(self, longUrl: str) -> str:
+		return "http://tinyurl.com/" + longUrl.upper()
+		
+
+	def decode(self, shortUrl: str) -> str:
+		"""Decodes a shortened URL to its original URL.
+		"""
+		return shortUrl[19:].lower()
+
+# Your Codec object will be instantiated and called as such:
+codec = Codec()
+url = "https://leetcode.com/problems/design-tinyur"
+print(codec.decode(codec.encode(url)))
